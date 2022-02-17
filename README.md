c339414d3b82eb1e3a22026cb19c248268c5b2cb
STEF G <stefano@stefs-mbp.mynetworksettings.com>
2022-02-16 19:09:48 -0500

    Implementing errors with then/catch

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 [1 - Http Requests - Project setup ](../../tree/00ba1f2698d60e34d2cf54bae5368d871943b4dc/)
 
-[2 - Empty ](../../tree//)
+[2 - Added UI to inform the user ](../../tree/24b4a04adc663099359c1bf62d9a019051f192ad/)
 
 [3 - Empty ](../../tree//)
--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   function fetchMoviesHandler() {
     setIsLoading(true);
+    setError(null);
     fetch('https://swapi.dev/api/films')
       .then((response) => {
         return response.json();
+      })
+      .catch((err) => {
+        console.log(error);
       })
       .then((data) => {
         const transformedMovies = data.results.map((movieData) => {
