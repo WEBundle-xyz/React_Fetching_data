6ce8dae3c51af0eff0a3379006d26464de628d57
STEF G <stefano@stefs-mbp.mynetworksettings.com>
2022-02-16 19:20:16 -0500

    Changing from then/catch to async/await

--- README.md
+++ README.md
@@ -4,4 +4,10 @@
 
 [2 - Added UI to inform the user ](../../tree/24b4a04adc663099359c1bf62d9a019051f192ad/)
 
-[3 - Empty ](../../tree//)
+[3 - Implementing errors with then/catch ](../../tree/c339414d3b82eb1e3a22026cb19c248268c5b2cb/)
+
+[4 - Empty ](../../tree//)
+
+[5 - Empty ](../../tree//)
+
+[6 - Empty ](../../tree//)
--- src/App.js
+++ src/App.js
@@ -6,30 +6,23 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [error, serError] = useState(null);
 
-  function fetchMoviesHandler() {
+  async function fetchMoviesHandler() {
     setIsLoading(true);
-    setError(null);
-    fetch('https://swapi.dev/api/films')
-      .then((response) => {
-        return response.json();
-      })
-      .catch((err) => {
-        console.log(error);
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseData: movieData.release_data,
-          };
-        });
-        setMovies(transformedMovies);
-        setIsLoading(false);
-      });
+    const response = await fetch('https://swapi.dev/api/films/');
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -39,8 +32,8 @@ function App() {
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies</p>}
-        {isLoading && <p>Loading ... </p>}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
